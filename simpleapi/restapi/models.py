9e3f67f17abaa9174435e8c84a004c85ee573c6d
isakh <rafiisakh@gmail.com>
2017-12-13 15:03:05 +0700

    define model example

--- simpleapi/restapi/models.py
+++ simpleapi/restapi/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=200, blank=True, default='')
+    publish_date = models.DateTimeField()
+    book_category = models.CharField(max_length=200, blank=True, default='')
+    read = models.BooleanField(default=False)
+
+    class Meta:
+        ordering =  ('title',)
