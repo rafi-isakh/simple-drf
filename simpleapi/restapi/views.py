d755fcc8cca75ffd46975888d99241b2eb187ceb
isakh <rafiisakh@gmail.com>
2017-12-18 15:42:26 +0700

    class-based views

--- simpleapi/restapi/views.py
+++ simpleapi/restapi/views.py
@@ -3,57 +3,88 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
-from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import status, generics
 from restapi.models import Book
-from restapi.serializer import BookSerializer
+from restapi.models import BookCategory
+from restapi.serializer import BookSerializer, BookCategorySerializer
 
-# Create your views here.
-class JSONResponse(HttpResponse):
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
+# Function based view
 
-@csrf_exempt
-@api_view(['GET', 'POST'])
-def book_list(request):
-    if request.method == 'GET':
-        books = Book.objects.all()
-        book_serializer = BookSerializer(books, many=True)
-        return JSONResponse(book_serializer.data)
+# @csrf_exempt
+# @api_view(['GET', 'POST'])
+# def book_list(request):
+#     if request.method == 'GET':
+#         books = Book.objects.all()
+#         book_serializer = BookSerializer(books, many=True)
+#         return Response(book_serializer.data)
 
-    elif request.method == 'POST':
-        book_data = JSONParser().parse(request)
-        book_serializer = BookSerializer(data=book_data)
+#     elif request.method == 'POST':
+#         book_serializer = BookSerializer(data=request.data)
         
-        if book_serializer.is_valid():
-            book_serializer.save()
-            return JSONResponse(book_serializer.data, status=status.HTTP_201_CREATED)
+#         if book_serializer.is_valid():
+#             book_serializer.save()
+#             return Response(book_serializer.data, status=status.HTTP_201_CREATED)
 
-        return JSONResponse(book_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         return Response(book_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-@api_view(['GET', 'PUT', 'DELETE'])
-def book_detail(request, pk):
-    try:
-        book = Book.objects.get(pk=pk)
-    except Book.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+# @csrf_exempt
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def book_detail(request, pk):
+#     try:
+#         book = Book.objects.get(pk=pk)
+#     except Book.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
-        book_serializer = BookSerializer(book)
-        return JSONResponse(book_serializer.data)
+#     if request.method == 'GET':
+#         book_serializer = BookSerializer(book)
+#         return Response(book_serializer.data)
 
-    elif request.method == 'PUT':
-        book_data = JSONParser().parse(request)
-        book_serializer = BookSerializer(book, data=book_data)
+#     elif request.method == 'PUT':
+#         book_serializer = BookSerializer(book, data=request.data)
 
-        if book_serializer.is_valid():
-            book_serializer.save()
-            return JSONResponse(book_serializer.data)
+#         if book_serializer.is_valid():
+#             book_serializer.save()
+#             return Response(book_serializer.data)
 
-        return JSONResponse(book_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         return Response(book_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
-        book.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+#     elif request.method == 'DELETE':
+#         book.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# @csrf_exempt
+# @api_view(['GET', 'POST'])
+# def book_categories_list(request):
+#     if request.method == 'GET':
+#         book_categories = BookCategory.objects.all()
+#         book_serializer = BookCategorySerializer(book_categories, many=True)
+#         return Response(book_serializer.data)
+
+#     elif request.method == 'POST':
+#         book_serializer = BookCategorySerializer(data=request.data)
+        
+#         if book_serializer.is_valid():
+#             book_serializer.save()
+#             return Response(book_serializer.data, status=status.HTTP_201_CREATED)
+
+#         return Response(book_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# Class based views
+class BookCategoryList(generics.ListCreateAPIView):
+    queryset = BookCategory.objects.all()
+    serializer_class = BookCategorySerializer
+    name = 'gamecategory-list'
+
+class BookList(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    name = 'book-list'
+
+class BookDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    name = 'book-detail'
